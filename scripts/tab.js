7f26fbc1efb0d2a06a8e0e8b0ec1b5d8f57b92aa
AusraO <ausra.241@protonmail.com>
2023-04-18 15:14:50 +0300

    close button rotation, fixed tab styles

--- scripts/tab.js
+++ scripts/tab.js
@@ -2,6 +2,25 @@ const tab=document.querySelector('.tab')
 const openTab=document.querySelector('#intro');
 const closeTab=document.querySelector('.closeButton')
 
+
+const header=document.querySelector('.tabHeader');
+const headerEl=document.createElement('h1');
+const headerText=document.createTextNode('INTRO')
+headerEl.setAttribute('class', 'header')
+headerEl.appendChild(headerText)
+header.appendChild(headerEl)
+
+
+
+const videoBox=document.querySelector('.tabPicture')
+const video=document.createElement('video');
+video.setAttribute('src', 'resources/tab-1.mp4')
+video.setAttribute('type', 'video/mp4')
+video.setAttribute('class', 'video')
+videoBox.appendChild(video);
+
+
+
 openTab.addEventListener('click', ()=> {
     tab.showModal();
 })
